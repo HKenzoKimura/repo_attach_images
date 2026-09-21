c4f6e20f04274481167d530cd97db54419a65027
HKenzoKimura <henriquekkimura@gmail.com>
2026-09-21 11:44:19 -0300

    Add files via upload
    png

---
+++ image/verify_packagename.ps1
@@ -0,0 +1,12 @@
+﻿Get-ChildItem "Registry::HKEY_CLASSES_ROOT\Installer\Products" | ForEach-Object {
+    $sourceListPath = Join-Path $_.PSPath "SourceList"
+    if (Test-Path $sourceListPath) {
+        $pkg = Get-ItemProperty -Path $sourceListPath -Name PackageName -ErrorAction SilentlyContinue
+        if ($pkg) {
+            [PSCustomObject]@{
+                ProductKey  = $_.PSChildName
+                PackageName = $pkg.PackageName
+            }
+        }
+    }
+} | Where-Object { $_.PackageName -match "CsAgent" } #CsAgent
